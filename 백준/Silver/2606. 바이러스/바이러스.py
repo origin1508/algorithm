539515b0d14604bf369341aa1975f2e31c45676b
origin1508 <108377283+origin1508@users.noreply.github.com>
2022-10-31 11:14:09 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,14 +1,6 @@
 import sys
 input = sys.stdin.readline
 
-def dfs(graph, v, visited):
-  visited[v] = True
-  result.append(v)
-
-  for i in range(n+1):
-    if graph[v][i] == 1 and not visited[i]:
-      dfs(graph, i, visited)
-
 n = int(input().strip())
 m = int(input().strip())
 graph = [[0] *( n+1 )for _ in range(n+1)]
@@ -20,5 +12,13 @@ for _ in range(m):
   graph[r][c] = 1
   graph[c][r] = 1
 
-dfs(graph, 1, visited)
+def dfs(v):
+  visited[v] = True
+  result.append(v)
+
+  for i in range(n+1):
+    if graph[v][i] == 1 and not visited[i]:
+      dfs(i)
+
+dfs(1)
 print(len(result)-1)
