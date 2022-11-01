35f853f107fd7f3978a3d23787b0932dc569549f
origin1508 <108377283+origin1508@users.noreply.github.com>
2022-11-01 22:03:25 +0900

    [Silver IV] Title: 제로, Time: 108 ms, Memory: 31620 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+k = int(input())
+result = []
+for _ in range(k):
+  n = int(input())
+  if n == 0:
+    result.pop()
+  else:
+    result.append(n)
+
+print(sum(result))
