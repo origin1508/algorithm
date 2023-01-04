295f8043e55b0e81762d36e7b2d0255b0317b088
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-01-04 20:14:16 +0900

    [Silver I] Title: RGB거리, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -5,8 +5,8 @@ n = int(input())
 array = [list(map(int, input().rstrip().split())) for _ in range(n)]
 
 for i in range(1, n):
-  array[i][0] = min(array[i-1][1] + array[i][0], array[i-1][2] + array[i][0])
-  array[i][1] = min(array[i-1][0] + array[i][1], array[i-1][2] + array[i][1])
-  array[i][2] = min(array[i-1][0] + array[i][2], array[i-1][1] + array[i][2])
+  array[i][0] = min(array[i-1][1], array[i-1][2]) + array[i][0]
+  array[i][1] = min(array[i-1][0], array[i-1][2]) + array[i][1]
+  array[i][2] = min(array[i-1][0], array[i-1][1]) + array[i][2]
 
 print(min(array[n-1]))
