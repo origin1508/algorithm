58d16986398801c46276772e56401c0088f6f03d
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-02-10 14:52:22 +0900

    [unrated] Title: 둘만의 암호, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -0,0 +1,16 @@
+def solution(s, skip, index):
+    answer = ''
+    97, 122
+    list = [chr(i) for i in range(97, 123)]
+    for i in skip:
+        list.remove(i)
+    
+    def find(list, s, index):
+        order = (list.index(s) + index) % len(list)
+        return list[order]
+    
+    for i in s:
+        answer += find(list, i, index)
+    
+    return answer
+
