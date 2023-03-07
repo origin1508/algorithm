fb03878b8248c3ff689d3c61f3b807d8d33283dd
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-03-07 14:30:16 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = False
+    if len(s) == 4 or len(s) == 6:
+        try:
+            int(s)
+            answer = True
+        except:
+            answer = False
+        
+    return answer
