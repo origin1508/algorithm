0b4084844f8c1726d86a07f200eee58fbe6d56af
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-03-14 16:22:30 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,19 @@
+def solution(nums):
+    answer = 0
+    sums = []
+    
+    for i in range(len(nums)-2):
+        for j in range(i+1, len(nums)-1):
+            for k in range(j+1, len(nums)):
+                sums.append(nums[i] + nums[j] + nums[k])
+    
+    for i in sums:
+        check = True
+        for j in range(2, int(i ** 0.5) + 1):
+            if i % j == 0:
+                check = False
+                break
+        if check:
+            answer += 1
+    
+    return answer
