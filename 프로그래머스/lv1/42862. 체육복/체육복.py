bef6294df604c51c3ed37363386a84e439adb6a6
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-03-21 17:05:24 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,13 +1,15 @@
 def solution(n, lost, reserve):
-    set_lost = set(lost) - set(reserve)
-    set_reserve = set(reserve) - set(lost)
+    answer = 0
+    lost_set = set(lost) - set(reserve)
+    reserve_set = set(reserve) - set(lost)
+    student = [1] * (n + 1)
+    for l in lost_set:
+        if (l - 1) in reserve_set:
+            reserve_set.remove(l-1)
+            continue
+        if (l + 1) in reserve_set:
+            reserve_set.remove(l+1)
+            continue
+        student[l] = 0
     
-    for i in set_reserve:
-        if i-1 in set_lost:
-            set_lost.remove(i-1)
-        elif i+1 in set_lost:
-            set_lost.remove(i+1)
-    
-    answer = n - len(set_lost)
-    
-    return answer
+    return student.count(1) - 1
