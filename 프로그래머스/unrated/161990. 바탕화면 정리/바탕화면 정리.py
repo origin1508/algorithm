c88bb2e19a0864665194200513590a93e72e4c00
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-05-18 00:21:31 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.17 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,19 +1,3 @@
 def solution(wallpaper):
-    lux = 50
-    luy = 50
-    rdx = 0
-    rdy = 0
-    
-    for x in range(len(wallpaper)):
-        for y in range(len(wallpaper[0])):
-            if wallpaper[x][y] == "#":
-                if lux > x:
-                    lux = x
-                if luy > y:
-                    luy = y
-                if rdx < x + 1:
-                    rdx = x + 1
-                if rdy < y + 1:
-                    rdy = y + 1          
-
-    return [lux, luy, rdx, rdy]
+    answer = []
+    return answer
