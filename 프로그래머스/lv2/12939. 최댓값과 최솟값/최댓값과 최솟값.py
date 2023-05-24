68f22a1ba3bf26a4a66f2fd17957b7852b763ffe
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-05-24 16:10:19 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,6 +1,6 @@
 def solution(s):
     answer = []
-    s = list(s.split(" "))
+    s = s.split(" ")
     answer.append(min(s, key=lambda x: int(x)))
     answer.append(max(s, key=lambda x: int(x)))
     return " ".join(answer)
