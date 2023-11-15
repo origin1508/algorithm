57d934fc5cd5529cbf62f9e2096c01c158c56837
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-11-15 12:39:57 +0900

    [level unrated] Title: 분수의 덧셈, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,6 +1,6 @@
 function solution(numer1, denom1, numer2, denom2) {
     function getGCD(num1, num2) {
-        return num2 === 0 ? num1 : num1 > num2 ? getGCD(num2, num1 % num2) : getGCD(num1, num2 % num1)
+        return num2 === 0 ? num1 : getGCD(num2, num1 % num2)
     }
     
     const numer = numer1 * denom2 + numer2 * denom1
