a831987eabbce50c94dc07bed53ee8e0085ec1a5
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-11-16 15:59:57 +0900

    [level unrated] Title: 약수 구하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/unrated/120897. 약수 구하기/약수 구하기.js
@@ -1,9 +1,11 @@
 function solution(n) {
-    var answer = [];
-    for(let i = 1; i <= n; i++){
+    var answer = new Set();
+    for(let i = 1; i <= Math.sqrt(n); i++){
         if (n % i === 0) {
-            answer.push(i)
+            answer.add(i)
+            answer.add(n/i)
         }
     }
-    return answer;
+    
+    return [...answer].sort((a, b) => a - b);
 }
