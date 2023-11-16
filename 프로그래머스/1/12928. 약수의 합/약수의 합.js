b252a7910d1a7982b149585b5da171172f0ce714
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-11-17 01:00:16 +0900

    [level 1] Title: 약수의 합, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,4 +1,13 @@
 function solution(n) {
-    var answer = new Array(n).fill(0).map((_, index) => index + 1).filter(el => n % el === 0).reduce((acc, cur) => acc += cur, 0)
+    let answer = 0
+    for (let i = 1; i <= Math.sqrt(n) ; i++) {
+        if (n % i === 0) {
+            answer += i
+            if (i !== Math.sqrt(n)) {
+            answer += n / i
+                
+            }
+        }
+    }
     return answer;
 }
