45d356777b9d2957a8025a24b0b81107a709c20e
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-11-17 19:04:24 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,15 +1,4 @@
 function solution(my_string) {
-    var answer = 0;
-    let temp = 0;
-    for (let i of my_string) {
-        if (!isNaN(i)) {
-            temp *= 10
-            temp += Number(i)
-        } else {
-            answer += temp
-            temp = 0
-        }
-    }
-    if (temp) answer += temp
-    return answer;
-}
+    var answer = my_string.replace(/[a-z]/gi, " ").split(" ").reduce((acc, cur) => acc += Number(cur), 0)
+
+    return    answer}
