50980a1c6d6c67ffd39e8752ef16a7b4331885cb
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-11-17 11:46:37 +0900

    [level unrated] Title: A로 B 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120886. A로 B 만들기/A로 B 만들기.js
@@ -0,0 +1,11 @@
+function solution(before, after) {
+    const afterArr = [...after]
+    for (let i of before) {
+        const index = afterArr.indexOf(i)
+        if (index !== -1) afterArr.splice(index, 1)
+        else return 0
+        
+        if (afterArr.length === 0) return 1
+    }
+    return answer;
+}
