49884d76098266d692cc98652ab20e3f11265a2f
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-11-21 15:24:48 +0900

    [level unrated] Title: 안전지대, Time: 0.32 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120866. 안전지대/안전지대.js
@@ -0,0 +1,19 @@
+function solution(board) {
+    const checkDangerZone = (x, y) => {
+        const check = [[1, 0], [-1, 0], [0, 1], [0, -1], [-1, -1], [1, -1], [-1, 1], [1, 1]];
+        for (let i = 0; i < check.length; i++) {
+            const nextX = x + check[i][0]
+            const nextY = y + check[i][1]
+            if (nextX < 0 || nextX >= board.length) continue
+            if (nextY < 0 || nextY >= board[0].length) continue
+            if (board[nextX][nextY] !== 1) board[nextX][nextY] = 2
+        }
+    }
+    for (let x = 0; x < board.length; x++) {
+        for (let y = 0; y < board[0].length; y++) {
+            if (board[x][y] === 1) checkDangerZone(x, y)
+        }
+    }
+    var answer = board.reduce((acc, cur) => acc += cur.filter(el => el === 0).length,0)
+    return answer;
+}
