49525dc0faad7991bb0dd03e13ffe6e9dc42deae
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-11-21 20:43:55 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 68.34 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,11 @@
+function solution(arr)
+{
+    var answer = []
+    for (let i = 0; i < arr.length; i++) {
+        if (answer.at(-1) !== arr[i]) answer.push(arr[i])
+    }
+
+    
+    
+    return answer;
+}
