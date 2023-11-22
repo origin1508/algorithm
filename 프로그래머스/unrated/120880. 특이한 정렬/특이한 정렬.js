5eecd7e9215f558e4462c0df107a6254a653f324
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-11-22 18:24:17 +0900

    [level unrated] Title: 특이한 정렬, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/unrated/120880. 특이한 정렬/특이한 정렬.js
@@ -1,11 +1,4 @@
 function solution(numlist, n) {
-    var answer = [];
-    const distance = numlist.map((el) => [el, Math.abs(n - el)])
-    distance.sort((a, b) => {
-        if (a[1] - b[1] === 0) {
-            return b[0] - a[0]
-        } else return a[1] - b[1]
-    })
-    answer = distance.map(el => el[0])
-    return answer;
+    numlist.sort((a, b) => Math.abs(a - n) - Math.abs(b - n) || b - a)
+    return numlist;
 }
