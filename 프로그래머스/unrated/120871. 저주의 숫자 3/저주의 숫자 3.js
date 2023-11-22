4a66a1128808c92e4474b357fc0185c5ec95cae8
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-11-22 09:50:44 +0900

    [level unrated] Title: 저주의 숫자 3, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -2,7 +2,7 @@ function solution(n) {
     var answer = 1;
     for (let i = 1; i < n; i++) {
         answer++
-        while (answer % 3 === 0 || answer.toString().indexOf("3") !== -1) {
+        while (answer % 3 === 0 || answer.toString().includes("3")) {
             answer++
         }
     }
