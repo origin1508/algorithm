79bbaab9b57c0e1332ec6e7d4d811bd5856f4df9
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-11-24 13:00:24 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.64 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,5 +1,4 @@
 function solution(survey, choices) {
-    var answer = '';
     const type = [["R", "T"], ["C", "F"], ["J", "M"], ["A", "N"]]
     const result = {"R": 0,"T":0, "C": 0, "F": 0, "J": 0, "M": 0, "A": 0, "N": 0};
     survey.forEach((type, idx) => {
@@ -9,21 +8,7 @@ function solution(survey, choices) {
         else result[agree] += score;
     })
     
-    
-    
-    if (result["R"] >= result["T"]) answer += "R"
-    else answer += "T"
-    
-    if (result["C"] >= result["F"]) answer += "C"
-    else answer += "F"
-    
-    if (result["J"] >= result["M"]) answer += "J"
-    else answer += "M"
-    
-    if (result["A"] >= result["N"]) answer += "A"
-    else answer += "N"
-    
-    // type.map(([a, b]) => result[a] >= [result[b]] ? a : b).join("");
+    const answer = type.map(([a, b]) => result[a] >= [result[b]] ? a : b).join("");
     
     return answer
 }
