f3ebd8e845f98cebf20bb72ffa88e9c9642bd7dd
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-11-24 12:53:08 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.61 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -0,0 +1,24 @@
+function solution(survey, choices) {
+    var answer = '';
+    const result = {"R": 0,"T":0, "C": 0, "F": 0, "J": 0, "M": 0, "A": 0, "N": 0};
+    survey.forEach((type, idx) => {
+        const [disagree, agree] = type.split("");
+        const score = choices[idx] - 4;
+        if (score < 0) result[disagree] -= score;
+        else result[agree] += score;
+    })
+    
+    if (result["R"] >= result["T"]) answer += "R"
+    else answer += "T"
+    
+    if (result["C"] >= result["F"]) answer += "C"
+    else answer += "F"
+    
+    if (result["J"] >= result["M"]) answer += "J"
+    else answer += "M"
+    
+    if (result["A"] >= result["N"]) answer += "A"
+    else answer += "N"
+    
+    return answer;
+}
