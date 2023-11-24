600f6a72ec7a239f1d9a9e79b75b91b2bbcb8472
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-11-24 14:36:01 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 19.14 ms, Memory: 48 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -5,19 +5,19 @@ function solution(queue1, queue2) {
     let sum2 = getSum(queue2)
     let idx1 = 0;
     let idx2 = 0;
-    let limit = (queue1.length * 3)
+    let limit = (queue1.length * 3) - 1
     const total = sum1 + sum2
     if (total % 2 === 1) return -1;
     
     const half = total / 2
     while (sum1 !== sum2) {
-        if (sum1 > sum2 && sum2 < half) {
+        if (sum1 > sum2) {
             const el = queue1[idx1];
             queue2.push(el)
             idx1++
             sum1 -= el
             sum2 += el
-        } else if (sum2 > sum1 && sum1 < half) {
+        } else if (sum2 > sum1) {
             const el = queue2[idx2];
             queue1.push(el)
             idx2++
