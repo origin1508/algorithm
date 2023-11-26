491b04c0653c39adcd487c53db0862d9c4137506
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-11-27 00:06:47 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.08 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,7 +1,7 @@
 function solution(arr1, arr2) {
     var answer = arr1.map((el1, x) => {
         return el1.map((el2, y) => {
-            return el2 += arr2[x][y]
+            return el2 + arr2[x][y]
         })
     })
     return answer;
