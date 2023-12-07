448456430097090b5f4f1319796580afa7526d9f
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-12-07 12:54:02 +0700

    [level 1] Title: 카드 뭉치, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,14 +1,16 @@
 function solution(cards1, cards2, goal) {
     let answer = "Yes";
+    let idx1 = 0
+    let idx2 = 0
     for (let i = 0; i < goal.length; i++) {
         const target = goal[i]
-        if (cards1[0] === target) {
-            cards1.shift()
+        if (cards1[idx1] === target) {
+            idx1++
             continue
         }
         
-        if (cards2[0] === target) {
-            cards2.shift()
+        if (cards2[idx2] === target) {
+            idx2++
             continue
         }
         
