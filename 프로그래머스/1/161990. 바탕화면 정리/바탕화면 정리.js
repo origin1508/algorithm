84beb922642831d1f8ce25dda6bd1ad46dac14d9
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-12-21 18:38:04 +0700

    [level 1] Title: 바탕화면 정리, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -0,0 +1,14 @@
+function solution(wallpaper) {
+    var answer = [];
+    let start = [999, 999];
+    let end = [0, 0];
+    wallpaper.forEach((row, i) => {
+        [...row].forEach((el, j) => {
+            if (el === "#") {
+                start = [Math.min(start[0], i), Math.min(start[1], j)]
+                end = [Math.max(end[0], i + 1), Math.max(end[1], j + 1)]
+            }
+        })
+    })
+    return [...start, ...end];
+}
