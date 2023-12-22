36ccf09f3746c27004aab1616891b9763fbf0145
origin1508 <108377283+origin1508@users.noreply.github.com>
2023-12-22 23:41:12 +0900

    [level 1] Title: 달리기 경주, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/README.md
+++ 프로그래머스/1/178871. 달리기 경주/README.md
@@ -1,10 +1,10 @@
 # [level 1] 달리기 경주 - 178871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.23 ms
+메모리: 33.5 MB, 시간: 0.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 5일 23:33:58
+2023년 12월 5일 23:41:12
 
 ### 문제 설명
 
--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,49 +1,19 @@
 function solution(players, callings) {
     var answer = [];
-    let first = null
-    let prev = null
-    const playerList = {}
-    players.forEach((player) => {
-        playerList[player] = {
-            prev: prev,
-            next: null
-        }
-        if (prev === null) first = player
-        else {
-            playerList[prev].next = player
-        }
-        prev = player
+    const playerList = players.reduce((acc, cur, idx) => {
+        acc[cur] = idx
+        return acc
+    }, {})
+    
+    callings.forEach(call => {
+        const curIdx = playerList[call]
+        const nextIdx = curIdx - 1
+        const nextPlayer = players[nextIdx]
+        players[curIdx] = nextPlayer
+        players[nextIdx] = call
+        playerList[call]--
+        playerList[nextPlayer]++
     })
-
-    const swap = (call) => {
-        const prev = playerList[call].prev
-        const next = playerList[call].next
-        const prevPrev = playerList[prev].prev
-
-        if (prevPrev === null) {
-            first = call
-        } else {
-            playerList[prevPrev].next = call
-        }
-
-        if (next !== null) {
-            playerList[next].prev = prev
-        }
-
-        playerList[call].prev = prevPrev
-        playerList[call].next = prev
-        playerList[prev].prev = call
-        playerList[prev].next = next
-
-    }
-
-    callings.forEach(call => swap(call))
-
-    while(true) {
-        answer.push(first)
-        first = playerList[first].next
-        if (first === null) break
-    }
-
-    return answer;
+    
+    return players;
 }
