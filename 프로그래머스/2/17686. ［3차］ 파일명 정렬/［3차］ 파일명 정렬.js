af2504d36837aca4eea6b8a7cfa2d8b91561ef40
origin1508 <108377283+origin1508@users.noreply.github.com>
2024-02-03 12:03:44 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 12.55 ms, Memory: 39.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -0,0 +1,33 @@
+function solution(files) {
+    var answer = [];
+    const makePart = (file) => {
+        const fileLower = file.toLowerCase()
+        let head = ""
+        let number = ""
+        let findHead = false
+        for (let i = 0; i < fileLower.length; i++) {
+            const char = fileLower[i]
+            if (findHead && isNaN(char)) break
+            
+            if (isNaN(char) || char === " ") {
+                head += char
+            } else {
+                findHead = true
+                number += char
+            }
+        }
+        
+        return [head, Number(number)]
+    }
+    
+    files.sort((a, b) => {
+        const [headA, numberA] = makePart(a)
+        const [headB, numberB] = makePart(b)
+        
+        if (headA > headB) return 1
+        else if (headA < headB) return -1
+        else return numberA - numberB
+    })
+    
+    return files;
+}
