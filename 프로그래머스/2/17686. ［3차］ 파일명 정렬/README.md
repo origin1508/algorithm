a79f574da761fd68cd45ec770eb50d80858e5aa2
origin1508 <108377283+origin1508@users.noreply.github.com>
2024-02-03 12:13:21 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 7.72 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/README.md
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 파일명 정렬 - 17686 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17686) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17686#qna) 
 
 ### 성능 요약
 
-메모리: 39.2 MB, 시간: 12.55 ms
+메모리: 38 MB, 시간: 7.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 6일 12:3:44
+2024년 2월 6일 12:13:22
 
 ### 문제 설명
 
--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,22 +1,27 @@
 function solution(files) {
     var answer = [];
     const makePart = (file) => {
-        const fileLower = file.toLowerCase()
-        let head = ""
-        let number = ""
-        let findHead = false
-        for (let i = 0; i < fileLower.length; i++) {
-            const char = fileLower[i]
-            if (findHead && isNaN(char)) break
+//         const fileLower = file.toLowerCase()
+//         let head = ""
+//         let number = ""
+//         let findHead = false
+//         for (let i = 0; i < fileLower.length; i++) {
+//             const char = fileLower[i]
+//             if (findHead && isNaN(char)) break
             
-            if (isNaN(char) || char === " ") {
-                head += char
-            } else {
-                findHead = true
-                number += char
-            }
-        }
+//             if (isNaN(char) || char === " ") {
+//                 head += char
+//             } else {
+//                 findHead = true
+//                 number += char
+//             }
+//         }
+        const re = new RegExp(/(\D+)(\d+)/)
+        const matchedFile = file.match(re)
         
+        const head = matchedFile[1].toLowerCase()
+        const number = Number(matchedFile[2])
+
         return [head, Number(number)]
     }
     
