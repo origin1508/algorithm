64d2117433c129a9cee591ff514264e5a71c2c0e
origin1508 <108377283+origin1508@users.noreply.github.com>
2024-02-16 22:07:40 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 8.73 ms, Memory: 34.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -0,0 +1,46 @@
+function solution(m, n, board) {
+    let answer = 0;
+    const search = [[0, 0], [0, 1], [1, 1], [1, 0]]
+    const graph = board.map(el => [...el])
+    while (true) {
+        const container = new Set()
+        
+        for (let i = 0; i < m - 1; i++) {
+            for (let j = 0; j < n - 1; j++) {
+                const block = graph[i][j]
+                if (block === 0) continue
+
+                if (graph[i][j + 1] === block && graph[i + 1][j] === block && graph[i + 1][j + 1] === block) {
+                    container.add([i, j])
+                }
+            }
+        }
+        if (container.size === 0) break
+        
+        container.forEach(el => {
+            const x = Number(el[0])
+            const y = Number(el[1])
+            graph[x][y] = 0
+            graph[x][y + 1] = 0
+            graph[x + 1][y + 1] = 0
+            graph[x + 1][y] = 0
+        })
+        
+        for (let x = 1; x < m; x++) {
+            for (let y = 0; y < n; y++) {
+                if (graph[x][y] !== 0) continue
+                
+                for (let k = x; k > 0; k--) {
+                    if (graph[k - 1][y] !== 0) {
+                        [graph[k][y], graph[k - 1][y]] = [graph[k - 1][y], graph[k][y]]
+                    }
+                }
+            }
+        }
+    }
+    answer = graph.reduce((acc, cur) => {
+        return acc + cur.filter(el => el === 0).length
+    }, 0)
+    console.log(graph)
+    return answer;
+}
