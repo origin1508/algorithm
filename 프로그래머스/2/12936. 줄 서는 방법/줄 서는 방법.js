baf81698faeb1c207af29a52cb57641ef26b3660
origin1508 <108377283+origin1508@users.noreply.github.com>
2024-03-28 12:29:33 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
@@ -0,0 +1,18 @@
+function solution(n, k) {
+    const answer = [];
+    const numbers = new Array(n).fill(1).map((el, idx) => el + idx)
+    let order = k - 1
+    let total = numbers.reduce((acc, cur) => acc * cur, 1)
+
+    for (let i = n; i >= 1; i--) {
+        total /= i
+        const idx = Math.floor(order / total)
+        order %= total
+        const num = numbers[idx]
+        numbers.splice(idx, 1)
+        answer.push(num)
+        
+    }
+    
+    return answer;
+}
